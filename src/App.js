ec434c07c29aab5729d8ce1ed62d8e6b95e9769c
HussainShafayet <shafayetsec09@gmail.com>
2024-01-03 23:55:10 +0600

    install bootstrap and imported

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import './App.css';
 
 function App() {
   return (
-    <div></div>
+    <div>
+    
+    </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+//added bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
