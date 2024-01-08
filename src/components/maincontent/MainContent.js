5e3d0a101f0c1e4b811d9f23e608dd311eac4fcc
shafayet <shafayetsec09@gmail.com>
2024-01-09 01:16:23 +0600

    not found route added

--- src/components/maincontent/MainContent.js
+++ src/components/maincontent/MainContent.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import About from "./About";
 import Home from "./Home";
+import NotFound from "./Not-found";
 
 const MainContent = () => {
   return (
@@ -12,6 +13,9 @@ const MainContent = () => {
                 <Route path="/" element={<Home />} />
                 <Route path="/about" element={<About />} />
                 {/* Add more routes for other sections */}
+                {/* not found */}
+
+                <Route path="*" element={<NotFound />} />
             </Routes>
         </Router>
       {/* Add more sections as needed */}
---
+++ src/components/maincontent/Not-found.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function NotFound(props) {
+    return (
+        <div>Not Found</div>
+    );
+}
+
+export default NotFound;
