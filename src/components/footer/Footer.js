9406e523f1a6703b6ed32e14ed3bc7c1e1187455
HussainShafayet <shafayetsec09@gmail.com>
2024-01-15 01:55:30 +0600

    footer section update

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -3,9 +3,10 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer>
-      {/* Add footer content */}
-      <p>&copy; 2024 Your Portfolio</p>
+    <footer className='bg-body-secondary'>
+      <div className='w-100 h-100 d-flex justify-content-center align-items-center'>
+      <p>&copy; 2024 Shafayet Hussain. All rights reserved.</p>
+      </div>
     </footer>
   );
 };
