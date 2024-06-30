4cf4d28a9934c4163d15f1a6c6bda4eefdcab40c
shafayet <shafayetsec09@gmail.com>
2024-06-30 18:22:39 +0600

    title update in heaer

--- src/components/maincontent/Home.js
+++ src/components/maincontent/Home.js
@@ -11,7 +11,7 @@ function Home(props) {
             <div className={styles['header-content']}>
                 <h4 className={styles['header-subtitle']}>Hello, I am</h4>
                 <h1 className={styles['header-title']}>Shafayet Hussain</h1>
-                <h6 className={styles['header-description']}>Software Developer | Javascript | Ember</h6>
+                <h6 className={styles['header-description']}>Software Developer | Javascript | Ember | React</h6>
                 <button className="btn btn-primary btn-rounded"><i className="ti-printer pr-2"></i>Print Resume</button>
             </div>
         </div>
