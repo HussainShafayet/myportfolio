cb7efd3f7dd590a543654a55bfef59a1134138dc
shafayet <shafayetsec09@gmail.com>
2024-07-02 17:35:01 +0600

    project upadate

--- src/components/maincontent/Projects.js
+++ src/components/maincontent/Projects.js
@@ -12,17 +12,10 @@ const projects = [
     },
     {
       id: 2,
-      title: 'Image Gallery',
-      description: 'Simplify image management – upload, delete, and sort effortlessly for an organized gallery.',
-      image: 'image_gallery.png',
-      link: 'https://hussainshafayet.github.io/image-gallery',
-    },
-    {
-      id: 3,
-      title: 'Image Gallery',
-      description: 'Simplify image management – upload, delete, and sort effortlessly for an organized gallery.',
-      image: 'image_gallery.png',
-      link: 'https://hussainshafayet.github.io/image-gallery',
+      title: 'Todo App',
+      description: 'A Todo List Application built with React.js featuring a Kanban-style interface with three distinct columns: New, Ongoing, and Done. The app allows users to manage tasks through different stages of completion with added functionalities like context menus, date pickers, and form pre-filling.',
+      image: 'todo_app.png',
+      link: 'https://hussainshafayet.github.io/todo-app/',
     },
     // Add more project data as needed
   ];
